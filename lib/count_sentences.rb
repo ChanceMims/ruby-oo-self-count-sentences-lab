a53ff085c60ca663248f4a9b25444635e4b64618
Chance Mims <chance@Chances-MacBook-Air.local>
2019-12-10 22:44:24 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.split('')[-1] == '.'
   end
 
   def question?
-
+    self.split('')[-1] == '?'
   end
 
   def exclamation?
-
+    self.split('')[-1] == '!'
   end
 
   def count_sentences
-
+    length = 0
+    self.split(/[!.?]/) do |memo|
+      length += 1 if memo != ''
+    end
+    length
   end
 end
